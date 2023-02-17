dbf8c6baa313e2e3aa4c8fd117d7929b96b1708d
datsukan <s.datsukan@gmail.com>
2023-02-17 23:05:13 +0900

    ツイート情報をIDから取得するように変更

--- go.mod
+++ go.mod
@@ -7,3 +7,9 @@ require (
 	github.com/joho/godotenv v1.4.0
 	github.com/michimani/gotwi v0.13.0
 )
+
+require (
+	github.com/contentful-labs/contentful-go v0.4.1-0.20210618101201-b51913e15280 // indirect
+	github.com/datsukan/datsukan-blog-article-attribute v0.0.0-20230217134920-bdc4402c16de // indirect
+	moul.io/http2curl v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,31 +2,37 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	attribute "github.com/datsukan/datsukan-blog-article-attribute"
 	"github.com/joho/godotenv"
 	"github.com/michimani/gotwi"
 	"github.com/michimani/gotwi/tweet/managetweet"
 	"github.com/michimani/gotwi/tweet/managetweet/types"
 )
 
+// Input は、リクエスト情報の構造体。
+type Input struct {
+	Token string `json:"token"`
+	ID    string `json:"id"`
+}
+
 // ArticleInfo は、記事情報を定義した構造体。
 type ArticleInfo struct {
-	Token string `json:"token"`
-	Slug  string `json:"slug"`
-	Title string `json:"title"`
+	Slug  string
+	Title string
 }
 
 func main() {
 	t := flag.Bool("local", false, "ローカル実行か否か")
-	slug := flag.String("slug", "", "ローカル実行用の記事Slug")
-	title := flag.String("title", "", "ローカル実行用の記事Title")
+	id := flag.String("id", "", "ローカル実行用の記事ID")
 	flag.Parse()
 
-	isLocal, err := isLocal(t, slug, title)
+	isLocal, err := isLocal(t, id)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -34,11 +40,7 @@ func main() {
 
 	if isLocal {
 		fmt.Println("local")
-		ai := ArticleInfo{
-			Slug:  *slug,
-			Title: *title,
-		}
-		localController(ai)
+		localController(*id)
 		return
 	}
 
@@ -47,52 +49,89 @@ func main() {
 }
 
 // isLocal は、ローカル環境の実行であるかを判定する。
-func isLocal(t *bool, slug *string, title *string) (bool, error) {
+func isLocal(t *bool, id *string) (bool, error) {
 	if !*t {
 		return false, nil
 	}
 
-	if *slug == "" {
+	if *id == "" {
 		fmt.Println("no exec")
-		return false, fmt.Errorf("ローカル実行だがSlug指定が無いので処理不可能")
-	}
-
-	if *title == "" {
-		fmt.Println("no exec")
-		return false, fmt.Errorf("ローカル実行だがTitle指定が無いので処理不可能")
+		return false, fmt.Errorf("ローカル実行だがID指定が無いので処理不可能")
 	}
 
 	return true, nil
 }
 
 // localController は、ローカル環境での実行処理を行う。
-func localController(ai ArticleInfo) {
+func localController(id string) {
 	if err := godotenv.Load(); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	if err := useCase(ai); err != nil {
+	accessToken, spaceID, err := loadContentfulEnv()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	aa, err := attribute.New(id, accessToken, spaceID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if err := aa.Get(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ai := &ArticleInfo{
+		Slug:  aa.Slug,
+		Title: aa.Title,
+	}
+
+	if err := ai.useCase(); err != nil {
 		fmt.Println(err.Error())
+		return
 	}
 
 	fmt.Println("tweetしました。")
 }
 
 // controller は、API Gateway / AWS Step Functions / AWS Lambda 上での実行処理を行う。
-func controller(input ArticleInfo) error {
+func controller(input Input) error {
 	if input.Token != os.Getenv("API_TOKEN") {
 		return fmt.Errorf("unauthorized access")
 	}
 
-	if input.Slug == "" {
-		return fmt.Errorf("slug is empty")
-	}
-	if input.Title == "" {
-		return fmt.Errorf("title is empty")
+	if input.ID == "" {
+		return fmt.Errorf("id is empty")
 	}
 
-	if err := useCase(input); err != nil {
+	accessToken, spaceID, err := loadContentfulEnv()
+	if err != nil {
+		fmt.Println(err)
+		return fmt.Errorf("internal server error")
+	}
+
+	aa, err := attribute.New(input.ID, accessToken, spaceID)
+	if err != nil {
+		fmt.Println(err)
+		return fmt.Errorf("internal server error")
+	}
+
+	if err := aa.Get(); err != nil {
+		fmt.Println(err)
+		return fmt.Errorf("internal server error")
+	}
+
+	ai := &ArticleInfo{
+		Slug:  aa.Slug,
+		Title: aa.Title,
+	}
+
+	if err := ai.useCase(); err != nil {
 		return err
 	}
 
@@ -100,7 +139,8 @@ func controller(input ArticleInfo) error {
 }
 
 // useCase は、アプリケーションのIFに依存しないメインの処理を行う。
-func useCase(ai ArticleInfo) error {
+func (ai *ArticleInfo) useCase() error {
+
 	in := &gotwi.NewClientInput{
 		AuthenticationMethod: gotwi.AuthenMethodOAuth1UserContext,
 		OAuthToken:           os.Getenv("GOTWI_ACCESS_TOKEN"),
@@ -132,4 +172,17 @@ func tweet(c *gotwi.Client, text string) (string, error) {
 	}
 
 	return gotwi.StringValue(res.Data.ID), nil
+}
+
+// loadContentfulEnv は、 Contentful SDK の接続情報を環境変数から読み込む。
+func loadContentfulEnv() (string, string, error) {
+	accessToken := os.Getenv("CONTENTFUL_ACCESS_TOKEN")
+	spaceID := os.Getenv("CONTENTFUL_SPACE_ID")
+
+	if accessToken == "" || spaceID == "" {
+		m := fmt.Sprintf("environment variable not set [ accessToken: %v, spaceID: %v ]", accessToken, spaceID)
+		return "", "", errors.New(m)
+	}
+
+	return accessToken, spaceID, nil
 }
